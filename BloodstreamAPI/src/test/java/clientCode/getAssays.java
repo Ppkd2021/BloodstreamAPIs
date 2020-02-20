4ca2dbc06fa4c77a90eebdcd171d984d088b944d
Namita <nnaik@datainnovations.com>
2020-02-20 11:54:14 +0530

    Added few files

---
+++ BloodstreamAPI/src/test/java/clientCode/getAssays.java
@@ -0,0 +1,71 @@
+package clientCode;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.Hashtable;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import utilities.DataHandler;
+import utilities.config;
+
+public class getAssays {
+	
+	@BeforeTest
+	
+	public void PreTestProcess() 
+	{
+		RestAssured.basePath = config.property.getProperty("BasePathClientCode");
+		config.log.debug(new String(new char[100]).replace("\0", "-"));
+		config.log.debug(this.getClass().getName()+ " Entered");
+	}
+	//Validate 200 Status code 
+		@Test(dataProviderClass = DataHandler.class,dataProvider="dataProvider")
+		public void Assert200(Hashtable<String,String> dataTable) {
+			
+			config.log.debug(new Object() {}.getClass().getEnclosingMethod().getName()+ " Invoked");
+			String Authorization = config.property.getProperty("LoginToken");
+			String endpoint = dataTable.get("EndPoint");
+			
+		   given().relaxedHTTPSValidation().
+		   header("Authorization",Authorization).
+		   param("donationId",dataTable.get("donationId")).
+		   param("requestId",dataTable.get("requestId")).
+		   when().get(endpoint).then().     
+		   assertThat().statusCode(200); 
+					
+	 }
+	
+		 @Test(dataProviderClass = DataHandler.class,dataProvider="dataProvider")
+          public void Assert400(Hashtable<String,String> dataTable) {
+			
+			config.log.debug(new Object() {}.getClass().getEnclosingMethod().getName()+ " Invoked");
+			String Authorization = config.property.getProperty("LoginToken");
+			String endpoint = dataTable.get("EndPoint");
+		    given().relaxedHTTPSValidation().
+			header("Authorization",Authorization).
+			param("donationId",dataTable.get("donationId")).
+			param("requestId",dataTable.get("requestId")).
+			when().get(endpoint).then().     
+			assertThat().statusCode(400); 
+					
+	 }
+		 @Test(dataProviderClass = DataHandler.class,dataProvider="dataProvider")
+          public void Assert401(Hashtable<String,String> dataTable) {
+  			
+  			config.log.debug(new Object() {}.getClass().getEnclosingMethod().getName()+ " Invoked");
+  			String Authorization = config.property.getProperty("InvalidToken");
+  			String endpoint = dataTable.get("EndPoint");
+  		    given().relaxedHTTPSValidation().
+  			header("Authorization",Authorization).
+  			param("donationId",dataTable.get("donationId")).
+ 		   param("requestId",dataTable.get("requestId")).
+  			when().get(endpoint).then().     
+  			assertThat().statusCode(401); 
+  					
+  			 }
+  		
+}
+
---
+++ BloodstreamAPI/src/test/java/gridLayout/putDonationInfogridLayout.java
@@ -0,0 +1,68 @@
+package gridLayout;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.Hashtable;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import utilities.DataHandler;
+import utilities.config;
+
+public class putDonationInfogridLayout {
+
+	@BeforeTest
+	public void PreTestProcess() 
+	{
+		config.log.debug(new String(new char[100]).replace("\0", "-"));
+		config.log.debug(this.getClass().getName()+ " Entered");
+	}
+	//Validate 200 Status code 
+		@Test(dataProviderClass = DataHandler.class,dataProvider="dataProvider")
+		public void Assert200(Hashtable<String,String> dataTable) {
+			
+			config.log.debug(new Object() {}.getClass().getEnclosingMethod().getName()+ " Invoked");
+			String Authorization = config.property.getProperty("LoginToken");
+			String endpoint = dataTable.get("EndPoint");
+			
+		   given().relaxedHTTPSValidation().
+		   header("Authorization",Authorization).param("gridName",dataTable.get("gridName")).
+		   body(dataTable.get("Body")).
+		   when().put(endpoint).then().     
+		   assertThat().statusCode(200); 
+					
+	 }
+	
+		@Test(dataProviderClass = DataHandler.class,dataProvider="dataProvider")
+		public void Assert400(Hashtable<String,String> dataTable)
+		{
+			config.log.debug(new Object() {}.getClass().getEnclosingMethod().getName()+ " Invoked");
+			String Endpoint = dataTable.get("EndPoint");
+			String Authorization = "LoginToken";
+			
+			given().relaxedHTTPSValidation().
+			header("Authorization",Authorization). 
+			param("gridName",dataTable.get("gridName")).
+			
+			when().get(Endpoint).then().     
+			assertThat().statusCode(401);	
+		}
+		
+	 
+		  @Test(dataProviderClass = DataHandler.class,dataProvider="dataProvider")
+          public void Assert401(Hashtable<String,String> dataTable) {
+  			
+  			config.log.debug(new Object() {}.getClass().getEnclosingMethod().getName()+ " Invoked");
+  			String Authorization = config.property.getProperty("InvalidToken");
+  			String endpoint = dataTable.get("EndPoint");
+  		    given().relaxedHTTPSValidation().
+  			header("Authorization",Authorization).
+  			param("gridName",dataTable.get("gridName")).
+  			
+  			when().get(endpoint).then().      
+  			assertThat().statusCode(401); 
+  					
+  			 }
+  		
+}
